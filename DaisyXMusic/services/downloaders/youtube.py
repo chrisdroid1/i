4952c4df97cbb77ca8e732c032ddb7c5638919ec
chrisdroid1 <82050423+chrisdroid1@users.noreply.github.com>
2021-07-07 19:12:15 +0100

    Update youtube.py

--- DaisyXMusic/services/downloaders/youtube.py
+++ DaisyXMusic/services/downloaders/youtube.py
@@ -1,21 +1,3 @@
-# Calls Music 1 - Telegram bot for streaming audio in group calls
-# Copyright (C) 2021  Roj Serbest
-
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as
-# published by the Free Software Foundation, either version 3 of the
-# License, or (at your option) any later version.
-
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
-# Modified by Inukaasith
-
 from os import path
 
 from youtube_dl import YoutubeDL
@@ -39,12 +21,8 @@ def download(url: str) -> str:
 
     if duration > DURATION_LIMIT:
         raise DurationLimitError(
-            f"❌ Videos longer than {DURATION_LIMIT} minute(s) aren't allowed, the provided video is {duration} minute(s)"
-        )
-    try:
-        ydl.download([url])
-    except:
-        raise DurationLimitError(
-            f"❌ Videos longer than {DURATION_LIMIT} minute(s) aren't allowed, the provided video is {duration} minute(s)"
-        )
+            f"❌ Videos longer than {DURATION_LIMIT} minute(s) aren't allowed, "
+            f"the provided video is {duration} minute(s)",
+       )
+    ydl.download([url])
     return path.join("downloads", f"{info['id']}.{info['ext']}")
